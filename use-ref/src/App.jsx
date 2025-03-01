1a5f8969d620eb77d9c807b05b7451112dc8c7b1
MogesBekele <mogesbekele32@gmail.com>
2025-03-01 22:28:05 +0300

    commit

--- use-ref/src/App.jsx
+++ use-ref/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const App = () => {
+  const ref = useRef(null)
+  const [text, setText] = useState('moges')
+
+  const handleButton =()=>{
+    if(ref.current.value){
+      setText(ref.current.value)
+    } else {
+      alert('Please enter a value')
+    }
+
+  }
+
+
+
   return (
     <div>
+
+      <input type="text" ref={ref}  />
+
+      <button onClick={handleButton()}>Get Text</button>
+      <p>{text}</p>
+
       
     </div>
   )
