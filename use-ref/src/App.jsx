ca5f4391a17a72d0378fc86e76677aa212f26b0c
MogesBekele <mogesbekele32@gmail.com>
2025-03-02 20:42:07 +0300

    commit

--- use-ref/src/App.jsx
+++ use-ref/src/App.jsx
@@ -2,24 +2,8 @@ import React, { useRef, useState } from "react";
 import FetchData from "./FetchData";
 
 const App = () => {
-  const ref = useRef(null);
-  const [text, setText] = useState("moges");
-
-  const handleButton = () => {
-    if (ref.current.value) {
-      setText(ref.current.value);
-      alert("excellent");
-    }
-    ref.current.value = ""; // Clear input field after alert is displayed.
-    
-  };
-
   return (
     <div>
-      <input type="text" ref={ref} />
-
-      <button onClick={handleButton}>Get Text</button>
-      <p>{text}</p>
       <FetchData />
     </div>
   );
