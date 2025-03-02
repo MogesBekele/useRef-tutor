ecf491ef2f1a63f1a47d4b95f805f65f4508195e
MogesBekele <mogesbekele32@gmail.com>
2025-03-02 21:29:31 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import { uploadController } from "./uploadController.js";
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config(); // Load environment variables
 
@@ -14,6 +16,12 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+// Ensure the uploads directory exists
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
