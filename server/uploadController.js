6a8242609291cb9308f66538690ee97ba54338ed
MogesBekele <mogesbekele32@gmail.com>
2025-03-02 21:13:29 +0300

    commit

--- server/uploadController.js
+++ server/uploadController.js
@@ -1,5 +1,23 @@
-export const uploadController = (req, res) => {
+import User from "./models/User.js";
+
+export const uploadController = async (req, res) => {
   const { name, email, password } = req.body;
-  console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
-  res.send(`File uploaded successfully: ${req.file.filename}`);
+  const filename = req.file.filename;
+
+  try {
+    const newUser = new User({
+      name,
+      email,
+      password,
+      filename,
+    });
+
+    await newUser.save();
+
+    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+    res.send(`File uploaded successfully: ${filename}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
 };
