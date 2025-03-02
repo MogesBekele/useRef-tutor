9b2050f0369d082e065ffbb87d6abc78ac9d0e58
MogesBekele <mogesbekele32@gmail.com>
2025-03-02 20:13:49 +0300

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,26 @@
 import mongoose from "mongoose";
 
+mongoose.connect("mongodb://localhost/mydatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => console.error(err));
+
+db.once("open", () => console.log("Connected to MongoDB"));
+
+const userSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true, unique: true},
+  image: {type: String, required: true, unique: true},
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+export default User;
+
