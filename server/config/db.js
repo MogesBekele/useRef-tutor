25e529a705ec5030d1c709abb6a0c6f0dcc57383
MogesBekele <mogesbekele32@gmail.com>
2025-03-02 20:16:48 +0300

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,26 +1,28 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost/mydatabase", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Database connection function
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected");
+  });
+  await mongoose.connect(`${process.env.MONGODB_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
 
-const db = mongoose.connection;
+export { connectDB };
 
-db.on("error", (err) => console.error(err));
-
-db.once("open", () => console.log("Connected to MongoDB"));
-
+// User schema definition
 const userSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true, unique: true},
-  image: {type: String, required: true, unique: true},
-
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  image: { type: String, required: true },
 });
 
+// User model creation
 const User = mongoose.model("User", userSchema);
-
 
 export default User;
 
