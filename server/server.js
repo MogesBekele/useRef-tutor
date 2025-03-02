332659a6f05904b3fb60db795dfe4a9f93439cb7
MogesBekele <mogesbekele32@gmail.com>
2025-03-02 20:07:29 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,39 +1,33 @@
-import express from 'express';
-import multer from 'multer';
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+app.use(cors());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   res.send(`File uploaded successfully: ${req.file.filename}`);
 });
 
-
-
-
-const app = express();
-const port = 4000
-
-app.use(express.json());
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.listen(()=>{
+app.listen(() => {
   console.log(`Server is running on port ${port} `);
- 
 });
-
-
-
