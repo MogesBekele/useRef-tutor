9bad724be6bb8f31d92311574f1e7934688fd207
MogesBekele <mogesbekele32@gmail.com>
2025-03-02 21:24:29 +0300

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const mongoUri = process.env.MONGO_URI;
+    console.log("Mongo URI:", mongoUri); // Add this line to debug
+
+    await mongoose.connect(mongoUri, {
       // useNewUrlParser: true, // Remove this line
       // useUnifiedTopology: true, // Remove this line
     });
